36dbae09f397d90ef1fd6032162ce4207653b745
Frank S. Thomas <frank@timepit.eu>
2016-02-17 22:17:49 +0100

    Add RenderingEngine.Pandoc

--- src/main/scala/org/mdoc/common/model/RenderingEngine.scala
+++ src/main/scala/org/mdoc/common/model/RenderingEngine.scala
@@ -4,6 +4,7 @@ sealed abstract class RenderingEngine extends Product with Serializable
 
 object RenderingEngine {
   case object LibreOffice extends RenderingEngine
+  case object Pandoc extends RenderingEngine
   case object Wkhtmltoimage extends RenderingEngine
   case object Wkhtmltopdf extends RenderingEngine
 }
